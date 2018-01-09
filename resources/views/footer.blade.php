36eb81f131b846685ee4f1a31c0649196de161de
ori-volfo <ori.volfo@gmail.com>
2018-01-09 20:06:51 +0200

    CSS footer - switched to flexbox

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,10 +1,10 @@
 @section('footer')
     <footer class="footer">
-        <div class="row">
-            <div class="col-xs-12 col-sm-5 copyright footer-item">
+        <div class="flex-container">
+            <div class="flex-item">
                 Copyright &copy; 2018 Sisense Inc. All rights reserved.
             </div>
-            <div class="col-xs-12 col-sm-7">
+            <div class="flex-item">
                 <ul class="nav navbar-nav navbar-right footer-item">
                     <li><a href="#">Terms of Use</a></li>
                     <li><a href="#">Sitemap</a></li>
